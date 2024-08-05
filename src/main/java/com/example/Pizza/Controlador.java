514b6f80fd5ad6ac60377f8df228661dc477287f
Hotzuul <polancopuquiosa@gmail.com>
2024-08-05 15:11:09 -0500

    Primer commit login

---
+++ src/main/java/com/example/Pizza/Controlador.java
@@ -0,0 +1,14 @@
+package com.example.Pizza;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controlador {
+    
+    @GetMapping("/")
+    public String inicio(Model model) {
+        return "login";
+    }
+}
